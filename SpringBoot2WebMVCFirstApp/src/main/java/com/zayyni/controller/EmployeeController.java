cbf15adddaf37dd6564ed39095df69ff1cad8e73
zayyni <zainulislam.733@gmail.com>
2025-03-17 22:37:49 +0500

    worked on different embedded servers

---
+++ SpringBoot2WebMVCFirstApp/src/main/java/com/zayyni/controller/EmployeeController.java
@@ -0,0 +1,15 @@
+package com.zayyni.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class EmployeeController {
+	
+	
+	@GetMapping("/show")
+	public String showPage() {
+		return "EmpHome";
+	}
+
+}
