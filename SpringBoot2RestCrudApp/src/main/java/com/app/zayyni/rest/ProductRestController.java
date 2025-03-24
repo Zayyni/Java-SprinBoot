27abbd67f93e37a7b303251247876117f3aea7e1
zayyni <zainulislam.733@gmail.com>
2025-03-24 23:37:08 +0500

    spring project with logging implementation

--- SpringBoot2RestCrudApp/src/main/java/com/app/zayyni/rest/ProductRestController.java
+++ SpringBoot2RestCrudApp/src/main/java/com/app/zayyni/rest/ProductRestController.java
@@ -2,6 +2,8 @@ package com.app.zayyni.rest;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +20,9 @@ import com.app.zayyni.service.IProductService;
 @RequestMapping("/products")
 public class ProductRestController {
 	
+	
+	private static final Logger LOG = LoggerFactory.getLogger(ProductRestController.class);
+	
 	@Autowired
 	private IProductService service;
 	
@@ -25,9 +30,31 @@ public class ProductRestController {
 	public ResponseEntity<String> saveProduct(
 			@RequestBody Product product)
 	{
-		Integer id = service.saveProduct(product);
+		LOG.info("Entered Into saveProduct method");
+		ResponseEntity<String> resp = null;
+		try {
+			Integer id = service.saveProduct(product);
+			resp = new ResponseEntity<String>(
+					new StringBuffer()
+					.append("Product '")
+					.append(id)
+					.append("' saved")
+					.toString(),
+					HttpStatus.CREATED
+					);
+			LOG.info("Product Saved with ID {}",id);
+			
+		} catch (Exception e) {
+			LOG.error("Unable to save product {}",e.getMessage());
+			resp = new ResponseEntity<String>(
+				"Unable to process Save Product",
+				HttpStatus.INTERNAL_SERVER_ERROR
+				);
+			e.printStackTrace();
+		}
 		
-		return new ResponseEntity<String>("Product Saved "+id,HttpStatus.OK);
+		LOG.info(" ABOUT TO LEAVE SAVE PRODUCT METHOD ");
+		return resp;
 	}
 	
 	
