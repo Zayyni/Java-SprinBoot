98ca8996a61628d76a51980ac2d4dc9453bfdffd
zayyni <zainulislam.733@gmail.com>
2025-04-25 23:39:57 +0500

    implemented config server for microservies appliaction for using commong application properties

---
+++ SpringCloudCSEmployeeApp/src/main/java/com/app/zayyni/rest/EmployeeRestController.java
@@ -0,0 +1,20 @@
+package com.app.zayyni.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeRestController {
+	
+	
+	@Value("${my.app.title}")
+	private String title;
+	
+	
+	@GetMapping("/msg")
+	public String showMsg() {
+		return "WELCOME TO APP => " + title;
+	}
+
+}
