3b073402b8fab3f9f43ba3bb360f6fd537965705
zayyni <zainulislam.733@gmail.com>
2025-05-01 23:03:40 +0500

    implement apache active mq basic by implememting the connection factory

---
+++ SpringBoot2JmsEx/src/main/java/com/app/zayyni/config/AppConfig.java
@@ -0,0 +1,22 @@
+package com.app.zayyni.config;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import jakarta.jms.ConnectionFactory;
+
+@Configuration
+public class AppConfig {
+	
+	@Bean
+	public ConnectionFactory conn() {
+		
+		ActiveMQConnectionFactory am = new ActiveMQConnectionFactory();
+		am.setBrokerURL("tcp://localhost:61616");
+		am.setUserName("admin");
+		am.setPassword("admin");
+		return am;
+	}
+
+}
