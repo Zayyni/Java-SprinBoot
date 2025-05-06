051fd0c6fff8410d28fc71388bb1f69dd98065a2
zayyni <zainulislam.733@gmail.com>
2025-05-06 22:51:55 +0500

    integrated springboot with kafka

---
+++ SpringBoot2KafkaIntegration/src/main/java/com/app/zayyni/SpringBoot2KafkaIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.app.zayyni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2KafkaIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot2KafkaIntegrationApplication.class, args);
+	}
+
+}
