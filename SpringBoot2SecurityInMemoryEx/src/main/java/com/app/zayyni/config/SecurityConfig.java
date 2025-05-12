8fa1ab5f2d7a96e0591d96868c573c49b6d83fb2
zayyni <zainulislam.733@gmail.com>
2025-05-12 22:19:54 +0500

    implemented spring security in springboot

---
+++ SpringBoot2SecurityInMemoryEx/src/main/java/com/app/zayyni/config/SecurityConfig.java
@@ -0,0 +1,9 @@
+package com.app.zayyni.config;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+public class SecurityConfig extends WebSecurityConfiguration{
+	
+	
+
+}
