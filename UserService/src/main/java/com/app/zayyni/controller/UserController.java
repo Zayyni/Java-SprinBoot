74900a3b51cffdbf10c96f19ee717e51798ac8a5
zayyni <zainulislam.733@gmail.com>
2025-05-16 22:11:05 +0500

    implemented new user service with rest controller

---
+++ UserService/src/main/java/com/app/zayyni/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.app.zayyni.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+	
+	@GetMapping("/greet")
+	public String greet() {
+		return "Hello There !!!!";
+	}
+
+}
