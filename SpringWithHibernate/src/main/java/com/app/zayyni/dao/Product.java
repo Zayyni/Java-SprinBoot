02cf33c086b18e20a6b08396f8c2b89e7fae9d38
zayyni <zainulislam.733@gmail.com>
2025-05-22 22:30:33 +0500

    implemented simple class with hibernate

---
+++ SpringWithHibernate/src/main/java/com/app/zayyni/dao/Product.java
@@ -0,0 +1,28 @@
+package com.app.zayyni.dao;
+
+public class Product {
+	
+	private Integer pid;
+	private String name;
+	private Integer quantity;
+	public Integer getPid() {
+		return pid;
+	}
+	public void setPid(Integer pid) {
+		this.pid = pid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+	
+
+}
