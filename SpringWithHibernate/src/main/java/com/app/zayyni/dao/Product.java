5296eabbc0bcd631c1594c762cee9f17fd1d4189
zayyni <zainulislam.733@gmail.com>
2025-05-26 21:41:57 +0500

    Added hibernate core maven dependency for jpa management

--- SpringWithHibernate/src/main/java/com/app/zayyni/dao/Product.java
+++ SpringWithHibernate/src/main/java/com/app/zayyni/dao/Product.java
@@ -1,10 +1,13 @@
 package com.app.zayyni.dao;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+
 @Entity
-public class Product {
+public class Product implements Serializable{
 	
 	@Id
 	private Integer pid;
