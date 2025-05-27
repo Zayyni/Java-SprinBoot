c9181459bb5046a98041575c5db8308be05544bb
zayyni <zainulislam.733@gmail.com>
2025-05-27 22:18:02 +0500

    implemented user service using built in method of crudrepo interface

--- UserService/src/main/java/com/app/zayyni/service/UserService.java
+++ UserService/src/main/java/com/app/zayyni/service/UserService.java
@@ -16,20 +16,20 @@ public class UserService {
 	@Autowired
 	UserRepository repo;
 	
-List<User> ul = new ArrayList<>();
+//List<User> ul = new ArrayList<>();
 	
-	public UserService() {
-		ul.add(new User(1,"ALI","Lhr","35784856"));
-		ul.add(new User(2,"USMAN","Khr","254385"));
-		ul.add(new User(3,"ZIA","Isl","98936893"));
-		ul.add(new User(4,"ZOHAIB","Fsd","687368768"));
-		ul.add(new User(5,"JOHN","Lhr","9389368936"));
-		ul.add(new User(6,"MIKE","Lhr","9389368936"));
-		ul.add(new User(7,"JOHN","Lhr","93287246"));
-		ul.add(new User(8,"Jef","Fsd","875826"));
-		ul.add(new User(9,"JOHN","Lhr","93287246"));
-		ul.add(new User(10,"Jef","Fsd","875826"));
-	}
+//	public UserService() {
+//		ul.add(new User(1,"ALI","Lhr","35784856"));
+//		ul.add(new User(2,"USMAN","Khr","254385"));
+//		ul.add(new User(3,"ZIA","Isl","98936893"));
+//		ul.add(new User(4,"ZOHAIB","Fsd","687368768"));
+//		ul.add(new User(5,"JOHN","Lhr","9389368936"));
+//		ul.add(new User(6,"MIKE","Lhr","9389368936"));
+//		ul.add(new User(7,"JOHN","Lhr","93287246"));
+//		ul.add(new User(8,"Jef","Fsd","875826"));
+//		ul.add(new User(9,"JOHN","Lhr","93287246"));
+//		ul.add(new User(10,"Jef","Fsd","875826"));
+//	}
 	
 	public List<User> getUsers(){
 //		return ul;
@@ -43,11 +43,11 @@ List<User> ul = new ArrayList<>();
 		return repo.findById(uid).orElseThrow(()->new UserNotFoundException("User not found with id "+ uid));
 	}
 	
-	public User getUserbyName(String uname) {
-		return ul.stream().filter(u->u.getUname().equalsIgnoreCase(uname))
-				.findFirst()
-				.orElseThrow(()->new UserNotFoundException("User not found with name "+ uname));
-	}
+//	public User getUserbyName(String uname) {
+//		return ul.stream().filter(u->u.getUname().equalsIgnoreCase(uname))
+//				.findFirst()
+//				.orElseThrow(()->new UserNotFoundException("User not found with name "+ uname));
+//	}
 	
 	public User insertUser(User usr) {
 //		ul.add(usr);
