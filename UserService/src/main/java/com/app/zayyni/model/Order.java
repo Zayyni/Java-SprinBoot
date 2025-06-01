926c48b8ea4f3200f4bfefba3eca40ff3f825484
zayyni <zainulislam.733@gmail.com>
2025-06-01 22:02:47 +0500

    implemented custom id generation uisng strategy springdata jpa methods

--- UserService/src/main/java/com/app/zayyni/model/Order.java
+++ UserService/src/main/java/com/app/zayyni/model/Order.java
@@ -3,6 +3,8 @@ package com.app.zayyni.model;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -15,6 +17,7 @@ import lombok.NoArgsConstructor;
 public class Order {
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer oid;
 	private String name;
 	private String status;
